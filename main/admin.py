9662fdfb1fb94907efa710c2c442e65eb654b989
Theodore Keloglou <zf@sirodoht.com>
2024-07-19 19:58:12 +0100

    add base models

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,59 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DjUserAdmin
 
-# Register your models here.
+from main import models
+
+
+@admin.register(models.User)
+class UserAdmin(DjUserAdmin):
+    list_display = (
+        "id",
+        "username",
+        "email",
+    )
+    search_fields = ("username", "email")
+    ordering = ["-id"]
+
+
+@admin.register(models.Persona)
+class PersonaAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "created_at",
+        "prompt",
+    )
+    search_fields = (
+        "id",
+        "prompt",
+    )
+    ordering = ["-id"]
+
+
+@admin.register(models.Policy)
+class PolicyAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "created_at",
+        "text",
+    )
+    search_fields = (
+        "id",
+        "text",
+    )
+    ordering = ["-id"]
+
+
+@admin.register(models.Reaction)
+class ReactionAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "policy",
+        "persona",
+        "prompt",
+    )
+    search_fields = (
+        "id",
+        "prompt",
+        "text",
+    )
+    ordering = ["-id"]
--- main/models.py
+++ main/models.py
@@ -1,3 +1,31 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.username
+
+
+class Persona(models.Model):
+    created_at = models.DateTimeField(default=timezone.now)
+    prompt = models.TextField()
+
+
+class Policy(models.Model):
+    created_at = models.DateTimeField(default=timezone.now)
+    text = models.TextField()
+
+    class Meta:
+        verbose_name_plural = "Policies"
+
+
+class Reaction(models.Model):
+    created_at = models.DateTimeField(default=timezone.now)
+    policy = models.ForeignKey(Policy, on_delete=models.CASCADE)
+    persona = models.ForeignKey(Persona, on_delete=models.CASCADE)
+    prompt = models.TextField()
+    text = models.TextField()
