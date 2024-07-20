3c23b2a11e1d4b98a1557ec3810134c855a2d332
Theodore Keloglou <zf@sirodoht.com>
2024-07-20 14:56:53 +0100

    add person and past simulation lists on index

--- main/views.py
+++ main/views.py
@@ -10,6 +10,12 @@ class SimulationCreate(CreateView):
     fields = ["policy"]
     template_name = "main/index.html"
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["persona_list"] = models.Persona.objects.all()
+        context["simulation_list"] = models.Simulation.objects.all().order_by("-id")
+        return context
+
     def get_success_url(self):
         return reverse("simulation_detail", kwargs={"pk": self.object.pk})
 
